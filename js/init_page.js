4162d5b6c354b5bad23435e5d29dc56fe311b6ef
parpar8090 <parpar.lego@gmail.com>
2021-03-17 15:58:36 +0000

    c

--- js/init_page.js
+++ js/init_page.js
@@ -1,6 +1,4 @@
-const url = "https://test.parpar8090.com";
-$(document).ready(function(){
-  $('#head').load(url+"head.html"); 
-  $('#header').load(url+"/header.html"); 
-  $('#footer').load(url+"footer.html"); 
+$(document).ready(function(){ 
+  $(".header").load("header.html"); 
+  $(".footer").load("footer.html"); 
 });
