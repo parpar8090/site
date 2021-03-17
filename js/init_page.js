e7bca682f442b7ef03be0ada1b121df2f7c450b4
parpar8090 <51650998+parpar8090@users.noreply.github.com>
2021-03-16 22:16:28 -0500

    b

--- js/init_page.js
+++ js/init_page.js
@@ -1,6 +1,6 @@
 const url = "https://test.parpar8090.com";
-JQuery(document).ready(function(){
-  JQuery('#head').load(url+"head.html"); 
-  JQuery('#header').load(url+"/header.html"); 
-  JQuery('#footer').load(url+"footer.html"); 
+$(document).ready(function(){
+  $('#head').load(url+"head.html"); 
+  $('#header').load(url+"/header.html"); 
+  $('#footer').load(url+"footer.html"); 
 });
